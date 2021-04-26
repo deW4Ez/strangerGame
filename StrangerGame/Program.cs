9c8005a6c1e70f6883aa0cbabc59045a72be8665
deW4Ez <ilkarik@gmail.com>
2021-04-26 13:58:58 +0500

    first try

---
+++ StrangerGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StrangerGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
